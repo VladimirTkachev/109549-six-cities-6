3de305a8ff09d0f240f4fe6fec70b0e5e1204f22
Vladimir Tkachev <vladimirtkachev1991@gmail.com>
2021-02-05 21:56:48 +0300

    module2-task1: Link from NotFoundPage to MainPage

--- src/components/not-found-page/NotFoundPage.jsx
+++ src/components/not-found-page/NotFoundPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <h2>
-      404 Not Found
-    </h2>
+    <>
+      <h2>
+        404 Not Found
+      </h2>
+      <Link to="/">
+        Нажмите, чтобы перейти на главную страницу
+      </Link>
+    </>
   );
 };
 
