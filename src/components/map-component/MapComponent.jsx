78da810c45ffae91b1c10eda3c0fb65141d6421d
Vladimir Tkachev <vladimirtkachev1991@gmail.com>
2021-02-18 23:43:05 +0300

    module4-task1: MapComponent coderef

--- src/components/map-component/MapComponent.jsx
+++ src/components/map-component/MapComponent.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PropTypes from "prop-types";
 import leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -8,12 +8,13 @@ import {OfferCardTypes} from "Project/prop-types/offer-card";
 const MapComponent = (props) => {
   const {items, activeItem} = props;
   const mapRef = useRef(null);
+  const [mapSettings, setMapSettings] = useState(null);
   const [firstItem = {}] = items;
 
   useEffect(() => {
     const city = firstItem.city;
 
-    mapRef.current = leaflet.map(mapRef.current, {
+    const settings = leaflet.map(mapRef.current, {
       center: {
         lat: city.location.latitude,
         lng: city.location.longitude
@@ -25,15 +26,17 @@ const MapComponent = (props) => {
       .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
         attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
       })
-      .addTo(mapRef.current);
+      .addTo(settings);
+
+    setMapSettings(settings);
 
     return () => {
       mapRef.current.remove();
     };
-  }, []);
+  }, [mapRef, firstItem, setMapSettings]);
 
   useEffect(() => {
-    if (mapRef.current) {
+    if (mapSettings) {
       items.forEach((item) => {
         const isActive = activeItem ? item.id === activeItem.id : false;
         const city = item.city;
@@ -49,14 +52,14 @@ const MapComponent = (props) => {
         {
           icon: customIcon
         })
-        .addTo(mapRef.current)
+        .addTo(mapSettings)
         .bindPopup(city.name);
       });
     }
-  }, [items, activeItem]);
+  }, [items, activeItem, mapSettings]);
 
   return (
-    <div id="map" style={{height: `100%`}} ref={mapRef}/>
+    <div style={{height: `100%`}} ref={mapRef}/>
   );
 };
 
