19cf45271b41e75119c17c299e5a532e5470b481
Vladimir Tkachev <vladimirtkachev1991@gmail.com>
2021-02-26 21:51:53 +0300

    module6-task2: fix import

--- src/components/private-route/hocs/with-private-route.js
+++ src/components/private-route/hocs/with-private-route.js
@@ -1,10 +1,10 @@
 import {connect} from "react-redux";
 
-import {getAuth} from "Project/redux/selectors";
+import {getAuthStatus} from "Project/redux/selectors";
 
 function mapStateToProps(state) {
   return {
-    authStatus: getAuth(state),
+    authStatus: getAuthStatus(state),
   };
 }
 
